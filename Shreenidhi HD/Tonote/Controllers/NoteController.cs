21a9dc69e4c9fdf2081fd9fd655c51e13ef82d85
Shreenidhi H D <58159026+ShreenidhiHD@users.noreply.github.com>
2021-11-18 04:49:02 +0530

    some changes are made

---
+++ Shreenidhi HD/Tonote/Controllers/NoteController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tonote.Models;
+
+namespace Tonote.Controllers
+{
+    public class NoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Note()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Note(NoteModule obj)
+        {
+            ViewBag.Msg = "The Note Name" + obj.Name + "added";
+            return View();
+        }
+    }
+}
---
+++ Shreenidhi HD/Tonote/Models/NoteModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Tonote.Controllers;
+
+namespace Tonote.Models
+{
+    public class NoteModule
+    {
+        // [Required(ErrorMessage ="")]
+        public String NoteId { get; set; }
+        [Required(ErrorMessage = "Note Name is required")]
+        public String Name { get; set; }
+        
+    }
+}
