a92e0c5de82d959746647d78c66246af8600580d
Shreenidhi H D <58159026+ShreenidhiHD@users.noreply.github.com>
2021-12-17 16:45:09 +0530

    Todo assignment
    hey this is new folder please clone it

---
+++ Todo_asignment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Todo_asignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
