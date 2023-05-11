c0f1ee1e31452b847366e92e25e59874cf2ac6d3
rajguptaH <rajkumar00999.rk@gmail.com>
2023-05-11 14:52:13 +0530

    gitignore update and created a db proj

--- Api/SmartdustApi/SmartdustApi/Controllers/HomeController.cs
+++ Api/SmartdustApi/SmartdustApi/Controllers/HomeController.cs
@@ -9,11 +9,6 @@ namespace SmartdustApi.Controllers
     [Route("[controller]")]
     public class HomeController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
         private readonly ILogger<HomeController> _logger;
         private readonly IContactService _contactService;
 
