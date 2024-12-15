2f308f9f4c101f1c649794883ce6fe3344e3e4e1
MuskanKumariMK <muskankumari00999.mk@gmail.com>
2024-12-15 17:48:12 +0530

    Make ml model with Deep neural network

---
+++ Muskan/Ml_.Net/Ml_Models_In_VS_Zip_file/Ml_Models_In_VS/Models/FlowerOutput.cs
@@ -0,0 +1,7 @@
+﻿namespace Ml_Models_In_VS.Models
+{
+    public class FlowerOutput
+    {
+        public string[] species { get; set; }
+    }
+}
---
+++ Muskan/Ml_.Net/Ml_Models_In_VS_Zip_file/Ml_Models_In_VS/Models/ResumePredictionResult.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Ml_Models_In_VS.Models
+{
+    public class ResumePredictionResult
+    {
+        [JsonProperty("resume_name")]
+        public string ResumeName { get; set; }
+
+        [JsonProperty("predicted_category")]
+        public string PredictedCategory { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("confidence_score")]
+        public float ConfidenceScore { get; set; }
+    }
+
+    public class ResumePredictionResponse
+    {
+        [JsonProperty("predicted_categories")]
+        public List<ResumePredictionResult> PredictedCategories { get; set; }
+    }
+}
